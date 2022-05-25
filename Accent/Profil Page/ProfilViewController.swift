effd0a454d913fd66cde1a0eb1b91dba31705565
mnooransyari <mnooransyari@gmail.com>
2022-05-25 14:26:18 +0800

    Initial Commit

---
+++ Accent/Profil Page/ProfilViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfilViewController.swift
+//  Accent
+//
+//  Created by Muhammad Noor Ansyari on 25/05/22.
+//
+
+import UIKit
+
+class ProfilViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
